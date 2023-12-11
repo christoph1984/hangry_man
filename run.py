f51f1b0ab852ae0d03409eee48b2dfdd31777f41
christoph1984 <>
2023-12-11 20:33:40 +0000

    Add in basic functionality to select a random word from the list

--- run.py
+++ run.py
@@ -4,3 +4,11 @@ def hangryman():
   word_list = ["Spaghetti", "Guacamole", "Biscotti", "Zucchini", "Tiramisu", "Hamburger", "Croissant", "Quesadilla",
     "Sushi", "Barbecue", "Cheesecake", "Risotto", "Pancakes", "Kimchi", "Ratatouille", "Falafel", "Gazpacho",
     "Macarons", "Goulash", "Bruschetta"]
+
+    random_number = random.randint(0, len(word_list)-1)
+    chosen_word = word_list[random_number]
+    guess_word = []
+    for letter in chosen_word:
+        guess_word.append("_")
+    print("The word you need to guess has", len(chosen_word), "characters")
+    print(' '.join(guess_word))

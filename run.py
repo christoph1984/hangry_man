71c029e43c6280d3e233ba9f10ee22875460b8d9
christoph1984 <>
2023-12-11 20:43:26 +0000

    Print statement added to check that randome word is selected from the list

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
 import random
 
 def hangryman():
-  word_list = ["Spaghetti", "Guacamole", "Biscotti", "Zucchini", "Tiramisu", "Hamburger", "Croissant", "Quesadilla",
-    "Sushi", "Barbecue", "Cheesecake", "Risotto", "Pancakes", "Kimchi", "Ratatouille", "Falafel", "Gazpacho",
-    "Macarons", "Goulash", "Bruschetta"]
+    word_list = ["Spaghetti", "Guacamole", "Biscotti", "Zucchini", "Tiramisu",
+                 "Hamburger", "Croissant", "Quesadilla", "Sushi", "Barbecue", 
+                 "Cheesecake", "Risotto", "Pancakes", "Kimchi",
+                 "Ratatouille", "Falafel", "Gazpacho",
+                 "Macarons", "Goulash", "Bruschetta"]
 
     random_number = random.randint(0, len(word_list)-1)
     chosen_word = word_list[random_number]
     guess_word = []
     for letter in chosen_word:
         guess_word.append("_")
-    print("The word you need to guess has", len(chosen_word), "characters")
-    print(' '.join(guess_word))
+    # Debug print statement
+    print("Debug: The chosen word is", chosen_word)
+    
+hangryman()
+
